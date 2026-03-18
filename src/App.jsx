cb1f37e35a0a2a9d58caf463400132560b6e6def
Kristina Yordanova <kristinayordanovaa@gmail.com>
2026-03-18 14:56:21 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
+
+
+const App = () => {
+    return ( 
+    <main>
+        <div>
+            <h1 className="text-indigo-600">My React App</h1>
+        </div>
+
+
+    </main> 
+);
+}
+ 
+export default App;
